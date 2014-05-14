082181185dc198f12d7c1cb10c42f39b0483b833
Stig Henning Fredheim <stighef@gmail.com>
2014-05-14 20:55:50 +0200

    Updating WCS bbox and fixing terrainWidth
    WCS bbox needs to cover the whole "Gløshaugen".
    (If using stored texture mode instead of WMS, the WCS bbox also needs to
    correspond with the texture bbox. This is the case after this update)
    In addition the terrainWidth as based on the verticesRatio, when it
    should have been length ratio. This did not affect non WCS mode, since
    the resolution there is equal in north/south and west/east

--- src/index.js
+++ src/index.js
@@ -122,7 +122,7 @@ var addTerrainAndBuildings = function() {
 	
 	if ( getParameterFromUrl('wcs') === "true" ) {
 		var wcsName = "wcsKartverket";
-		var bbox = "569900,7032300,570500,7033300";
+		var bbox = "569800,7032300,570500,7033300";
 		terrainImporter.importTerrainWCS( wcsName, bbox );
 	}
 	else {
